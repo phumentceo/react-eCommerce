75c2419be91d09858377735a9cacf7ac6ea5a717
phumentceo <roungchanrith8@gmail.com>
2025-02-18 23:58:01 +0700

    React Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-900">
+        Hello world!
+      </h1>
+
+      <p className=" font-semibold opacity-60">Build simple eccomerce with tailwindcss</p>
+    </>
+  )
+}
+
+export default App
